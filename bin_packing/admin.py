6e423f50c406bc71e08869f8ac23a3bf4769c34b
Vaibhav <vaibhavpaliwal620@gmail.com>
2024-06-07 19:32:53 +0530

    added jazzmin code

--- bin_packing/admin.py
+++ bin_packing/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Panel
 
-# Register your models here.
+
+@admin.register(Panel)
+class PanelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'csv_file', 'zip_file', 'created_at', 'updated_at']
+    search_fields = ['id']
+    list_filter = ['created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']

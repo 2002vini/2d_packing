5f7d90a440bcfd86abdb6f028da2cc631aa27e5f
Vini Hundlani <hundlanivini2002@gmail.com>
2024-06-12 00:52:14 +0530

    changed allowed host removed colon

--- marble_cutting/settings.py
+++ marble_cutting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xg!k#00*2)vuscw5*2jok5mee&r7gd00mg+et6!sb_139-o2=@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inanimarbles.pythonanywhere.com',]
+ALLOWED_HOSTS = ['inanimarbles.pythonanywhere.com']
 
 
 # Application definition

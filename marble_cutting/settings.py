6325860765fba0b27ef1083b924c2110b468f6ca
Vaibhav <vaibhavpaliwal620@gmail.com>
2024-06-14 12:44:06 +0530

    updated static_root

--- marble_cutting/settings.py
+++ marble_cutting/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # *** STATIC CONFIGURATIONS FOR DEVELOPMENT ***
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 MEDIA_URL = '/media/'

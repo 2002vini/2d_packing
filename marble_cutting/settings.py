ebbe7b2bbe06baff05da3e21051d96d4914102e7
Vaibhav <vaibhavpaliwal620@gmail.com>
2024-06-14 12:42:41 +0530

    updated static_url

--- marble_cutting/settings.py
+++ marble_cutting/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # ]
 
 # *** STATIC CONFIGURATIONS FOR DEVELOPMENT ***
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
